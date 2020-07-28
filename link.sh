2c5e0a819577c072d4680e9adcef5dd00fad9cc5
Zach Bai <10970845+zachbai@users.noreply.github.com>
2020-07-27 17:46:32 -0700

    Dont create nested dupe symlink

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@ CURRENT_DIRECTORY=$(pwd)
 ln -fs $CURRENT_DIRECTORY/tmux.conf $HOME/.tmux.conf
 ln -fs $CURRENT_DIRECTORY/vimrc $HOME/.vimrc
 ln -fs $CURRENT_DIRECTORY/vim $HOME/.vim
-ln -fs $CURRENT_DIRECTORY/nvim $HOME/.config/nvim
+ln -fs $CURRENT_DIRECTORY/nvim $HOME/.config/
