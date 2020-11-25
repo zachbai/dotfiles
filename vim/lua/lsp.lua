dbe26dae8fd98f3dec51870307d8da36700370e9
Zach Bai <10970845+zachbai@users.noreply.github.com>
2020-11-25 10:55:35 -0800

    LSP config updates.

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -11,6 +11,8 @@ vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 vim.g.completion_confirm_key = '<CR>'
 vim.g.completion_matching_ignore_case = true
 
+-- Enable LSP snippet support, which has integration with vim-snip.
+-- Snippet support is disabled by default.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
@@ -18,25 +20,37 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 nvimLsp.tsserver.setup{
   on_attach = onAttach,
   root_dir = nvimLsp.util.root_pattern(".git", "package.json"),
-  capabilities = capabilities
+  capabilities = capabilities,
+
+  -- Experimental config for testing test neovim branch (PR #13371).
+  flags = { allow_incremental_sync = true },
 }
 
 -- JSON
 nvimLsp.jsonls.setup{
   on_attach = onAttach,
-  capabilities = capabilities
+  capabilities = capabilities,
+
+  -- Experimental config for testing test neovim branch (PR #13371).
+  flags = { allow_incremental_sync = true },
 }
 
 -- Lua
 nvimLsp.sumneko_lua.setup{
   on_attach = onAttach,
-  capabilities = capabilities
+  capabilities = capabilities,
+
+  -- Experimental config for testing test neovim branch (PR #13371).
+  flags = { allow_incremental_sync = true },
 }
 
 -- Viml
 nvimLsp.vimls.setup{
   on_attach = onAttach,
-  capabilities = capabilities
+  capabilities = capabilities,
+
+  -- Experimental config for testing test neovim branch (PR #13371).
+  flags = { allow_incremental_sync = true },
 }
 
 -- Dart
@@ -51,8 +65,10 @@ nvimLsp.vimls.setup{
 --
 
 -- cpp
--- Use vim-lsc for cpp until https://github.com/neovim/neovim/issues/12972 is resolved.
 nvimLsp.clangd.setup{
   on_attach = onAttach,
-  capabilities = capabilities
+  capabilities = capabilities,
+
+  -- Experimental config for testing test neovim branch (PR #13371).
+  flags = { allow_incremental_sync = true },
 }
--- vim/plugin/lsp.vim
+++ vim/plugin/lsp.vim
@@ -1,5 +1,4 @@
 nnoremap <silent> <c-]> <cmd>lua vim.lsp.buf.definition()<CR>
-nnoremap <silent> <c-[> <cmd>lua vim.lsp.buf.implementation()<CR>
 nnoremap <silent> K <cmd>lua vim.lsp.buf.hover()<CR>
 nnoremap <silent> <leader>k <cmd>lua vim.lsp.buf.signature_help()<CR>
 nnoremap <silent> <leader>a <cmd>lua vim.lsp.buf.code_action()<CR>
@@ -8,8 +7,9 @@ nnoremap <silent> g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
 nnoremap <silent> gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
 nnoremap <silent> <leader>rn <cmd>lua vim.lsp.buf.rename()<CR>
 
-let g:diagnostic_enable_virtual_text = 1
-let g:diagnostic_auto_popup_while_jump = 0
-nnoremap <silent> g[ :PrevDiagnosticCycle<CR>
-nnoremap <silent> g] :NextDiagnosticCycle<CR>
-nnoremap <silent> ;e :OpenDiagnostic<CR>
+" Disable for now; hasn't been supported by dart-ls or clangd.
+"nnoremap <silent> <c-[> <cmd>lua vim.lsp.buf.implementation()<CR>
+
+nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
+nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+nnoremap <silent> ;e <cmd>lua vim.lsp.diagnostic.set_loclist()<CR>
