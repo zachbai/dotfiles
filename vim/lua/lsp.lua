f15d8cd8cdea82a8a125a4a0e68ba3b3d1514f87
Zach Bai <zachrybai@gmail.com>
2022-08-11 16:14:03 -0700

    Use nvim-cmp for autocompletes.

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -1,8 +1,32 @@
 local nvimLsp = require"lspconfig";
+local cmp = require'cmp';
 
-local onAttach = function(client)
-  require'completion'.on_attach(client)
-end
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+       vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+       --vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+     --completion = cmp.config.window.bordered(),
+     --documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
 
 -- Completion settings
 vim.o.completeopt='menuone,noinsert,noselect'
@@ -15,6 +39,7 @@ vim.g.completion_matching_ignore_case = true
 -- Snippet support is disabled by default.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+require('cmp_nvim_lsp').update_capabilities(capabilities);
 
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
 
@@ -25,7 +50,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 -- TS/JS
 nvimLsp.tsserver.setup{
-  on_attach = onAttach,
   root_dir = nvimLsp.util.root_pattern(".git", "package.json"),
   capabilities = capabilities,
 
@@ -35,7 +59,6 @@ nvimLsp.tsserver.setup{
 
 -- JSON
 nvimLsp.jsonls.setup{
-  on_attach = onAttach,
   capabilities = capabilities,
 
   -- Experimental config for testing test neovim branch (PR #13371).
@@ -44,7 +67,6 @@ nvimLsp.jsonls.setup{
 
 -- Lua
 nvimLsp.sumneko_lua.setup{
-  on_attach = onAttach,
   capabilities = capabilities,
 
   -- Experimental config for testing test neovim branch (PR #13371).
@@ -53,7 +75,6 @@ nvimLsp.sumneko_lua.setup{
 
 -- Viml
 nvimLsp.vimls.setup{
-  on_attach = onAttach,
   capabilities = capabilities,
 
   -- Experimental config for testing test neovim branch (PR #13371).
@@ -63,7 +84,6 @@ nvimLsp.vimls.setup{
 -- Dart
 -- Use vim-lsc for dart until https://github.com/neovim/neovim/issues/12972 is resolved.
 --nvimLsp.dartls.setup{
-  --on_attach = onAttach;
   --init_options = {
     --onlyAnalyzeProjectsWithOpenFiles = "true",
     --suggestFromUnimportedLibraries = "false",
@@ -73,7 +93,6 @@ nvimLsp.vimls.setup{
 
 -- cpp
 nvimLsp.clangd.setup{
-  on_attach = onAttach,
   capabilities = capabilities,
 
   -- Experimental config for testing test neovim branch (PR #13371).
@@ -82,7 +101,6 @@ nvimLsp.clangd.setup{
 
 -- rust
 nvimLsp.rust_analyzer.setup{
-  on_attach = onAttach,
   capabilities = capabilities,
   ["rust-analyzer"] = {
     checkOnSave = {
